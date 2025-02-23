47fcddf503cbacfc94ebc270e595d10325c23bcc
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2025-02-23 16:08:08 +0400

    firebase setup and setp enverment variable.

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,81 @@
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile
+  } from 'firebase/auth'
+  import { GoogleAuthProvider } from 'firebase/auth'
+  import { createContext, useEffect, useState } from 'react'
+import auth from '../Firebase/auth'
 
-
-const AuthProvider = () => {
+  
+  // eslint-disable-next-line react-refresh/only-export-components
+  export const AuthContext = createContext()
+  
+  // eslint-disable-next-line react/prop-types
+  const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loder, setLoder] = useState(true)
+  
+    // Create User Using Email And Password
+    const createUser = (email, password) => {
+      return createUserWithEmailAndPassword(auth, email, password)
+    }
+  
+    // update User profile
+    const updateUserprofile = (name, photo) => {
+      return updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: photo
+      })
+    }
+  
+    // Login user with email & password
+    const loginUser = (email, password) => {
+      return signInWithEmailAndPassword(auth, email, password)
+    }
+  
+    // Login user with google
+    const loginWithGoogle = () => {
+      const provider = new GoogleAuthProvider()
+      return signInWithPopup(auth, provider)
+    }
+  
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, currentUser => {
+        setUser(currentUser)
+        console.log('state capture', currentUser)
+        setLoder(false)
+      })
+  
+      return () => {
+        unsubscribe()
+      }
+    }, [])
+  
+    // user logout
+    const userLogOut = () => {
+      return signOut(auth)
+    }
+  
+    const authInfo = {
+      user,
+      setUser,
+      loder,
+      setLoder,
+      createUser,
+      updateUserprofile,
+      loginUser,
+      loginWithGoogle,
+      userLogOut
+    }
+  
     return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default AuthProvider;
+      <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    )
+  }
+  
+  export default AuthProvider
+  
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/tailwindcss.css'
 import Router from './Router/Router'
-
+import AuthProvider from './Provider/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router></Router>
-  </StrictMode>,
+    <AuthProvider>
+      <Router></Router>
+    </AuthProvider>
+  </StrictMode>
 )
