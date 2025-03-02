6a219e01ba19ba7c24797afe5b745c988b65d1d7
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2025-03-02 12:49:32 +0400

    Add some style and implement the google login functionality.

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,5 +1,5 @@
 import { Form, Input, Button, Checkbox, message } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { UserOutlined, LockOutlined, GoogleOutlined } from '@ant-design/icons'
 import { useContext, useState } from 'react'
 import '../assets/css/Login.css'
 import { AuthContext } from '../Provider/AuthProvider'
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 const LoginPage = () => {
   const [isLogin, setIsLogin] = useState(true) // Toggle between login and signup
-  const { createUser, loginUser, updateUserprofile } = useContext(AuthContext)
+  const { createUser, loginUser, updateUserprofile, loginWithGoogle } = useContext(AuthContext)
 
   const handleLogin = values => {
     loginUser(values.email, values.password)
@@ -18,14 +18,29 @@ const LoginPage = () => {
         message.error('Signup faild!')
       })
   }
+
+  const handleGoogleLogin = () =>{
+    loginWithGoogle()
+    .then(()=>{
+      message.success('You are successfuly loged in.')
+    })
+    .catch(()=>{
+      message.error('Faild to login.')
+    })
+  }
   const handleSignup = values => {
     createUser(values.email, values.password)
       .then(() => {
         updateUserprofile(values.displayName, values.photoURL)
           .then(() => {
-            axios.post('https://task-management-application-server-side-six.vercel.app/user', values).then(() => {
-              message.success('Signup successful!')
-            })
+            axios
+              .post(
+                'https://task-management-application-server-side-six.vercel.app/user',
+                values
+              )
+              .then(() => {
+                message.success('Signup successful!')
+              })
           })
           .catch(() => {
             message.error('Signup faild!')
@@ -37,24 +52,26 @@ const LoginPage = () => {
   }
 
   return (
-    <div className='flex justify-center items-center bg-gradient-to-r min-h-screen'>
+    <div className='flex justify-center items-center min-h-screen'>
       <div className='flex bg-white shadow-2xl rounded-lg w-full max-w-4xl overflow-hidden'>
         {/* Image Section */}
         <div className='bg-cover bg-center w-1/2'>
-          <div className='flex justify-center items-center bg-black bg-opacity-50 h-full'>
+          <div className='flex justify-center items-center bg-gray-700 bg-opacity-50 h-full'>
             <div className='text-white text-center'>
-              <h1 className='mb-2 font-bold text-4xl'>Task Manager</h1>
-              <p className='text-lg'>Organize your tasks effortlessly.</p>
+              <h1 className='mb-2 font-bold text-6xl'>Task Vibe</h1>
+              <p className='mt-2 font-semibold text-lg'>
+                Organize your tasks effortlessly.
+              </p>
             </div>
           </div>
         </div>
 
         {/* Form Section */}
         <div className='p-8 w-1/2'>
-          <h2 className='mb-4 font-bold text-gray-800 text-2xl'>
+          <h2 className='mb-4 font-bold text-gray-800 text-3xl'>
             {isLogin ? 'Welcome Back!' : 'Create an Account'}
           </h2>
-          <p className='mb-6 text-gray-600'>
+          <p className='mb-6 text-gray-600 text-lg'>
             {isLogin
               ? 'Log in to manage your tasks.'
               : 'Sign up to get started with Task Manager.'}
@@ -163,6 +180,17 @@ const LoginPage = () => {
               </Form.Item>
             </Form>
           )}
+
+          <div>
+            <Button
+              style={{fontSize: '28px', padding: '20px'}}
+              htmlType='submit'
+              className='w-full'
+              onClick={handleGoogleLogin}
+            >
+              <GoogleOutlined /> <span className='font-semibold text-lg'>Login with google</span>
+            </Button>
+          </div>
 
           <div className='mt-4 text-center'>
             <span className='text-gray-600'>
